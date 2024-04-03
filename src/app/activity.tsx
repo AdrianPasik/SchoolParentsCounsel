a90b2d5eab2f8b55aaa3da21d26c7daa5470408c
Adrian Pasik <adrian.pasik@vp.pl>
2024-04-03 16:19:28 +0200

    code changes

---
+++ src/app/activity.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Activity() {
+    return (
+        <div className="flex group rounded border border-">
+        Test activity asdasd
+      </div>
+    )
+
+}
+
