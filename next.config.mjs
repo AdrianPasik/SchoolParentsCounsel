e97048ad6775d6753139cf129a4296df3a46de64
Adrian Pasik <adrian.pasik@vp.pl>
2024-09-11 11:01:30 +0200

    path fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    distDir: 'docs',
+    basePath: '/SchoolParentsCounsel',
+    assetPrefix: '/SchoolParentsCounsel/'
 };
 
 export default nextConfig;
