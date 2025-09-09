02e20b6b704e21aa401d39369db045bf2bdd5038
Adrian Pasik <AdrianPasik@users.noreply.github.com>
2025-09-09 21:33:54 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,14 +9,14 @@ export default function Home() {
                     <div className="w-full max-w-3xl mx-auto">
 
                         <div className="space-y-8 relative before:absolute before:inset-0 before:ml-5 before:-translate-x-px md:before:mx-auto md:before:translate-x-0 before:h-full before:w-0.5 before:bg-gradient-to-b before:from-transparent before:via-slate-300 before:to-transparent">
-                            <Activity Title="Dzień Chłopaka" Date="2024-09-30" Content="Przezenty dla chłopców." />
-                            <Activity Title="Dzień Nauczyciela" Date="2024-10-14" Content="Prezenty na dzień nauczyciela." />
-                            <Activity Title="Mikołajki" Date="2024-11-28" Content="Soki, pizza." />
-                            <Activity Title="Wigilia" Date="2024-12-24" Content="Kolacja wigilijna." />
-                            <Activity Title="Bal przebierańców" Date="2025-02-14" Content="Soki, pizza." />
-                            <Activity Title="Dzień kobiet" Date="2025-03-08" Content="Prezenty dla dziewczyn." />
-                            <Activity Title="Wielkanoc" Date="2025-04-18" Content="Spotkanie wigilijne." />
-                            <Activity Title="Koniec roku" Date="2025-06-27" Content="Prezenty dla nauczycieli itp." />
+                            <Activity Title="Dzień Chłopaka" Date="2025-09-30" Content="Przezenty dla chłopców." />
+                            <Activity Title="Dzień Nauczyciela" Date="2025-10-14" Content="Prezenty na dzień nauczyciela." />
+                            <Activity Title="Mikołajki" Date="2025-11-28" Content="Soki, pizza." />
+                            <Activity Title="Wigilia" Date="2025-12-24" Content="Kolacja wigilijna." />
+                            <Activity Title="Bal przebierańców" Date="2026-02-14" Content="Soki, pizza." />
+                            <Activity Title="Dzień kobiet" Date="2026-03-08" Content="Prezenty dla dziewczyn." />
+                            <Activity Title="Wielkanoc" Date="2026-04-18" Content="Spotkanie wigilijne." />
+                            <Activity Title="Koniec roku" Date="2026-06-27" Content="Prezenty dla nauczycieli itp." />
                             
                         </div>
 
